89b5595c893a94cf01fc2646236bc1d52307f20e
ErinDobbs <erinellepaige@gmail.com>
2024-12-10 23:02:54 +0000

    commit- m "submission"

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,99 @@
 import java.awt.*;
 
+//
+//  Class author: Erin Dobbs 
+//  Date created:  December 3 2024
+//  General description: Makes the ball for brick break 
+
+
 public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
 
+private int size;
+private int x;
+private int y;
+private int yVelocity;
+private int xVelocity;
+
+	//constructor(s):
+	public  Ball(int x, int y, int size){
+this.x = x;
+this.y = y;
+this.size = size;
+this.yVelocity = 1;
+this.xVelocity = 1;
+
+}
+
+public int getXpos() {
+	return x;
+}
+
+public void setX(int Xpos) {
+	this.x = Xpos;
+}
+
+public int getSize() {
+	return size;
+}
+
+public void setSize(int size) {
+	this.size = size;
+}
+
+
+
+
+public int getYpos() {
+	return y;
+}
+
+public void setY(int Ypos) {
+	this.y = Ypos;
+}
+
+public int getyVelocity() {
+	return yVelocity;
+}
+
+public void setYVelocity(int yVelocity) {
+	this.yVelocity = yVelocity;
+}
+
+public int getXVelocity() {
+	return xVelocity;
+}
+
+public void setXVelocity(int addVelocity) {
+	this.xVelocity = addVelocity;
+}
+
+public void move(){
+	x+= xVelocity;
+	y+= yVelocity;
+}
+
+public void reverseX(){
+	xVelocity = (-1* xVelocity);
+}
+
+public void reverseY(){
+
+	yVelocity = (-1 *yVelocity);
+}
+
+//
+//  Pre-condition: a postive number 
+//  Post-condition: draws the ball 
+
+//
+	//methods:
+	public void draw(Graphics g) {
+		g.setColor(Color.red); // call first to set the desired color
+		g.fillOval(x,y, size, size); // draws an oval (ball)
+
+	}
+	
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,104 @@
-import java.awt.Graphics;
 import java.awt.*;
+
+//
+//  Class author: Erin Dobbs 
+//  Date created:  December 3 2024
+//  General description: Makes the paddle for brick break 
+
 
 public class Paddle {
 
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
-	
+	private int Velocity;
+	private int Width;
+	private int Height;
+	private int Y;
+	private int X;
+	private int addVelocity;
+
 	//constructor(s):
-	
+public  Paddle(int X, int Y , int Width , int Height){
+this.Velocity = 1;
+this.X = X;
+this.Y = Y;
+this.Width = Width;
+this.Height = Height;
+this. addVelocity = 1;
+}
 	//methods:
+	public int getX() {
+		return X;
+	}
 	
+	public void setX(int X) {
+		this.X = X;
+	}
+	
+	public int getY() {
+		return Y;
+	}
+	
+	public void setY(int Y) {
+		this.Y = Y;
+	}
+	
+	public int getWidth() {
+		return Width;
+	}
+	
+	public void setWidth(int Width) {
+		this.Width = Width;
+	}
+	
+	public int getHeight() {
+		return Height;
+	}
+	
+	public void setHeight(int Height) {
+		this.Height= Height;
+	}
+	
+	public int getVelocity() {
+		return Velocity;
+	}
+	
+	public void setVelocity(int Velocity) {
+		this.Velocity = Velocity;
+	}
+
+
+	public void addVelocity(int toAdd) {
+	Velocity = Velocity + toAdd	;
+	}
+	
+
+//
+//  Pre-condition: a postive number 
+//  Post-condition: draws the paddle 
+
+//
+
+
+	public void draw(Graphics g) {
+		   g.setColor(Color.red); // call first to set the desired color
+               g.fillRect(X, Y, Width, Height); // draws a rectangle (paddle)
+
+	}
+//
+//  Pre-condition: there needs to be a velocity 
+//  Post-condition: moves the ball
+//
+
+
+	public void move(){
+		X+= Velocity;
+	}
+	
+
+
 	
 }
+
+
